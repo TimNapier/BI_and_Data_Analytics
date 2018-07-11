6ef3355d03ec8a308241f009d56362c1a602fc9f
TimNapier <tim.napier@hotmail.com>
2018-07-11 15:32:34 +1000

    Funder Type
    Added Derived Funder Type to CSM_Funder build

--- Scripts/snowsql/snowsql.sql
+++ Scripts/snowsql/snowsql.sql
@@ -2,5 +2,5 @@ use ARALUEN_DATAVAULT;
 use schema LOAD_NDIS;
 use warehouse ARALUEN;
 put file://C:\BI_and_Data_Analytics\Data\input_file.csv @STAGE_NDIS;
-COPY INTO "ARALUEN_DATAVAULT"."LOAD_NDIS"."CSM_Funder_Invoice_Detail" FROM @STAGE_NDIS file_format='LOAD_CSM.CSVFORMAT';
+COPY INTO "ARALUEN_DATAVAULT"."LOAD_NDIS"."CSM_Funder" FROM @STAGE_NDIS file_format='LOAD_CSM.CSVFORMAT';
 remove @STAGE_NDIS pattern='.*.csv.gz';
