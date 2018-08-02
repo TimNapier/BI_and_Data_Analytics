a4ebbf66fd1eb732125224ed280242b5ebc30b8d
TimNapier <tim.napier@hotmail.com>
2018-08-02 16:25:08 +1000

    Snow Flake Commands
    Wrote the Input File to the Workflow Directory, so it doesn't need coniguration on whatever machine its on, e.g. Araluen Server

--- Scripts/snowsql/snowsql.sql
+++ Scripts/snowsql/snowsql.sql
@@ -2,6 +2,6 @@ use ARALUEN_DATAVAULT;
 use schema LOAD_NDIS;
 use warehouse ARALUEN;
 CREATE OR REPLACE STAGE accounts FILE_FORMAT=LOAD_CSM.CSVFORMAT;
-put file://D:\BI\DATA\Snowflake_Upload\accounts.csv  @accounts;
+put file://D:\BI_and_Data_Analytics\Workflows\Source_Systems\Finance\MYOB\accounts\accounts.csv  @accounts;
 COPY INTO "ARALUEN_DATAVAULT"."LOAD_MYOB"."accounts" FROM @accounts file_format='LOAD_CSM.CSVFORMAT';
 remove @STAGE_NDIS pattern='.*.csv.gz';
