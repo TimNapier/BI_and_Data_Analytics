9717b0d3a6e9437091169e9a443f3ef96ff39c35
TimNapier <tim.napier@hotmail.com>
2018-08-06 14:54:17 +1000

    Service Centre Dashboard
    Journal Constellation

--- Scripts/snowsql/snowsql.sql
+++ Scripts/snowsql/snowsql.sql
@@ -1,7 +1,7 @@
 use ARALUEN_DATAVAULT;
 use schema LOAD_NDIS;
 use warehouse ARALUEN;
-CREATE OR REPLACE STAGE accounts FILE_FORMAT=LOAD_CSM.CSVFORMAT;
-put file://D:\BI_and_Data_Analytics\Workflows\Source_Systems\Finance\MYOB\accounts\accounts.csv  @accounts;
-COPY INTO "ARALUEN_DATAVAULT"."LOAD_MYOB"."accounts" FROM @accounts file_format='LOAD_CSM.CSVFORMAT';
+CREATE OR REPLACE STAGE Business Unit CSM Snowflake FILE_FORMAT=LOAD_CSM.CSVFORMAT;
+put file://D:\BI_and_Data_Analytics\Workflows\Source_Systems\Client Management\CSM\Business Unit CSM Snowflake\Business Unit CSM Snowflake.csv  @Business Unit CSM Snowflake;
+COPY INTO "ARALUEN_DATAVAULT"."LOAD_CSM"."Business Unit CSM Snowflake" FROM @Business Unit CSM Snowflake file_format='LOAD_CSM.CSVFORMAT';
 remove @STAGE_NDIS pattern='.*.csv.gz';
